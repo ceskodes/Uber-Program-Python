1e1a472043387acf25dda3c9968215dd54834841
Carlos Silva <ceskodes@gmail.com>
2022-05-27 19:15:40 -0400

    Agregando propiedades para los metodos y atributos

--- car.py
+++ car.py
@@ -4,3 +4,13 @@ class Car:
         self.license = license
         self.driver = driver
         self.passenger = passenger
+
+    @property
+    def passenger(self):
+        return self._passenger
+
+    @passenger.setter
+    def passenger(self, passenger):
+        if passenger != 4:
+            raise ValueError("Pasajeros no puede ser diferente a 4")
+        self._passenger = passenger
--- ubervan.py
+++ ubervan.py
@@ -3,4 +3,13 @@ from caradvanced import CarAdvanced
 class UberVan(CarAdvanced):
     def __init__(self, id, license, driver, passenger, typeCarAccepted, seatsMaterial):
         super().__init__(id, license, driver, passenger, typeCarAccepted, seatsMaterial)
-        # Passengers: 6
+    
+    @property
+    def passenger(self):
+        return self._passenger
+
+    @passenger.setter
+    def passenger(self, passenger):
+        if passenger != 6:
+            raise ValueError("Pasajeros no puede ser diferente a 6")
+        self._passenger = passenger
