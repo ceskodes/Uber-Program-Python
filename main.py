a14cf3f89bb3f45dc3fe73468a0071dece823a62
Carlos Silva <ceskodes@gmail.com>
2022-05-27 17:27:37 -0400

    Se elimina el getter de uberx y se agrega restriccion de pasajeros

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from user import User
 from driver import Driver
 
 if __name__ == "__main__":
-    Car1 = UberX(1, "AMS321", Driver(1, "Carlos Silva", "V25643915", "Caensi95@gmail.com", 1234), 3, "Chery", "Arauca")
-    print(Car1.get_driver())
+    Car1 = UberX(1, "AMS321", Driver(1, "Carlos Silva", "V25643915", "Caensi95@gmail.com", 1234), 4, "Chery", "Arauca")
+    print(vars(Car1))
--- uberx.py
+++ uberx.py
@@ -3,7 +3,7 @@ from carbasic import CarBasic
 
 class UberX(CarBasic):
     def __init__(self, id, license, driver, passenger, brand, model):
-        super().__init__(id, license, driver, passenger, brand, model)
-
-    def get_driver(self):
-        print(f'El conductor del automovil {self.brand} {self.model} es {vars(self.driver)}')
+        if passenger <= 4:
+            super().__init__(id, license, driver, passenger, brand, model)
+        else:
+            raise ValueError("No se pueden montar mas de 4 pasajeros")
