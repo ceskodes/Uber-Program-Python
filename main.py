c53dc99a251a74f259fc9c9e5fc454df6ac622bb
Carlos Silva <ceskodes@gmail.com>
2022-05-28 14:59:46 -0400

    Finalizado menu UBER en main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from card import Card
+from cash import Cash
+from paypal import Paypal
 from uberx import UberX
 from uberpool import UberPool
 from uberblack import UberBlack
@@ -5,6 +8,7 @@ from ubervan import UberVan
 from user import User
 from driver import Driver
 from trip import Trip
+from route import Route
 
 import random
 
@@ -12,6 +16,8 @@ if __name__ == "__main__":
 
     program_activated = 0
     payment_process = 0
+    driver_exist = False
+    user_exist = False
     aux = True
 
     # Inicio: Menu UBER
@@ -24,7 +30,7 @@ if __name__ == "__main__":
 
             # Entrada de datos
 
-            name_aux = input("Ingrese nombre del usuario: ")
+            name_aux = input("\nIngrese nombre del usuario: ")
             document_aux = input("Ingrese numero de identificacion: ")
             email_aux = input("Ingrese email: ")
             password_aux = input("Ingrese una contrasena: ")
@@ -32,13 +38,14 @@ if __name__ == "__main__":
             # Instanciando usuario (Solo 1 vez)
 
             User1 = User(1, name_aux, document_aux, email_aux, password_aux)
+            user_exist = True
             print(f'\nBienvenido {User1.get_name()}\n')
 
         elif program_activated == 2:
 
             # Entrada de datos
 
-            name_aux = input("Ingrese nombre del usuario: ")
+            name_aux = input("\nIngrese nombre del conductor: ")
             document_aux = input("Ingrese numero de identificacion: ")
             email_aux = input("Ingrese email: ")
             password_aux = input("Ingrese una contrasena: ")
@@ -47,42 +54,76 @@ if __name__ == "__main__":
             license_aux = input("Ingrese codigo de la licencia: ")
 
             while aux == True:
-                tipo_uber = int(input("Ingrese el tipo de Uber\n1. UberX\n2. UberPool\nOpcion: "))
+                tipo_uber = int(input("\nIngrese el tipo de Uber\n1. UberX\n2. UberPool\nOpcion: "))
             
                 # Instanciando conductor y vehiculo UBER (Solo 1 vez) (UberX o UberPool)
 
                 if tipo_uber == 1:
                     Driver1 = UberX(1, license_aux, Driver(1, name_aux, document_aux, email_aux, password_aux), 4, brand_car, model_car)
                     aux = False
+                    driver_exist = True
                 elif tipo_uber == 2:
                     Driver1 = UberPool(1, license_aux, Driver(1, name_aux, document_aux, email_aux, password_aux), 4, brand_car, model_car)
                     aux = False
+                    driver_exist = True
                 else:
                     print("\nIngrese una opcion valida\n")
 
-            print(f'Conductor {Driver1.driver.get_name()} registrado con exito!\n')
+            print(f'\nConductor {Driver1.driver.get_name()} registrado con exito!\n')
             aux = True
 
         elif program_activated == 3:
-            route_start = input("Ingrese ubicacion de origen: ")
-            route_end = input("Ingrese ubicacion de destino: ")
+            if driver_exist == True and user_exist == True:
+                # Route: Punto de origen y destino
+                route_start = input("\nIngrese ubicacion de origen: ")
+                route_end = input("Ingrese ubicacion de destino: ")
 
-            while payment_process != 4:
-                payment_process = int(input("\n--- Metodo de pago ---\n1. Efectivo\n2. Tarjeta\n3. PayPal\n4. Cancelar servicio\nOpcion: "))
-                if payment_process == 1:
-                    pass
-                elif payment_process == 2:
-                    pass
-                elif payment_process == 3:
-                    pass
-                else:
-                    print("\nIngrese una opcion valida\n")
+                # Costo del servicio
+                cost_aux = random.randint(5, 20)
+                print(f'\nEl costo del servicio es de ${cost_aux}.')
 
-                
-    
-    """
-    Car1 = UberX(1, "AMS321", Driver(1, "Carlos Silva", "V25643915", "Caensi95@gmail.com", 1234), 4, "Chery", "Arauca")
-    print(f'{Car1.driver.get_driver()} El vehiculo Uber es un {Car1.brand} {Car1.model} ')
+                # Metodo de pago
+                while payment_process != 4:
+                    payment_process = int(input("\n--- Metodo de pago ---\n1. Efectivo\n2. Tarjeta\n3. PayPal\n4. Cancelar servicio\nOpcion: "))
+                    
+                    # Efectivo
+                    if payment_process == 1:
+                        # Asignando trip al usuario
+                        User1.set_trip(Trip(Driver1, Route(1, route_start, route_end), Cash(1, cost_aux)))
+                        print(f'\nEl UBER asignado {Driver1.driver.get_name()} esta en camino a buscarlo en el punto: {route_start} y llegara en aproximadamente {random.randint(5, 8)} minutos.\n')
+                        break
 
-    Car2 = UberVan(2, "FTS123", Driver(2, "Luz Mar", "V5348227", "ramluzmar25@gmail.com", 5678), 6, "Daihatsu", "Terios")
-    print(vars(Car2))"""
+                    # Tarjeta
+                    elif payment_process == 2:
+
+                        # Entrada de datos para el pago
+                        card_number = int(input("\nIngrese el numero de la tarjeta: "))
+                        cvv_number = int(input("Ingrese el codigo CVV: "))
+                        exp_date = input("Ingrese fecha de vencimiento (Formato MM/AAAA): ")
+                        bank_card = input("Ingrese el banco asociado a la tarjeta: ")
+
+                        # Asignando trip al usuario
+                        User1.set_trip(Trip(Driver1, Route(1, route_start, route_end), Card(2, cost_aux, card_number, cvv_number, exp_date, bank_card)))
+                        print(f'\nSu pago fue realizado exitosamente, el UBER asignado {Driver1.driver.get_name()} esta en camino a buscarlo en el punto: {route_start} y llegara en aproximadamente {random.randint(5, 8)} minutos..\n')
+                        break
+
+                    # PayPal
+                    elif payment_process == 3:
+
+                        # Entrada de datos para el pago
+                        email_aux = input("\nIngrese su correo electronico de PayPal: ")
+
+                        # Asignando trip al usuario
+                        User1.set_trip(Trip(Driver1, Route(1, route_start, route_end), Paypal(1, cost_aux, email_aux)))
+                        print(f'\nSu pago fue realizado exitosamente, el UBER asignado {Driver1.driver.get_name()} esta en camino a buscarlo en el punto: {route_start} y llegara en aproximadamente {random.randint(5, 8)} minutos..\n')
+                        break
+
+                    # Opcion invalida
+                    elif payment_process == 4:
+                        print("\nCancelando servicio.\n")
+                    else:
+                        print("\nIngrese una opcion valida\n")
+
+                payment_process = 0
+            else:
+                print("\nDebes agregar un conductor o usuario primero.\n")
